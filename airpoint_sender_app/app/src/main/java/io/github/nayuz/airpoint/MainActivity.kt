0fb15ecc7504e75aa1bdaaaa96b325dc8a3aa556
Nayuz <nayuz.nil@gmail.com>
2025-01-14 14:28:14 +0900

    Add files with LFS tracking

---
+++ airpoint_sender_app/app/src/main/java/io/github/nayuz/airpoint/MainActivity.kt
@@ -0,0 +1,130 @@
+package io.github.nayuz.airpoint
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import io.github.nayuz.airpoint.camera.CameraXHelper
+import io.github.nayuz.airpoint.camera.OrientationHelper
+import io.github.nayuz.airpoint.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraXHelper: CameraXHelper
+    private lateinit var permissionHelper: PermissionHelper
+    private lateinit var handTrackerHelper : HandTrackerHelper
+    private lateinit var orientationHelper: OrientationHelper
+    //tcp 연결 요소
+    private lateinit var tcpConnectHelper: TcpConnectHelper
+    private lateinit var tcpAddressEditText: EditText
+    private lateinit var connectTcpButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //바인딩 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TCP 연결 UI 요소 초기화
+        tcpAddressEditText = findViewById(R.id.tcpSocketAddressEditText)
+        connectTcpButton = findViewById(R.id.connectButton)
+        tcpConnectHelper = TcpConnectHelper(this,binding.tcpSocketAddressEditText, binding.connectButton)
+
+        permissionHelper = PermissionHelper(this)
+        handTrackerHelper = HandTrackerHelper(this, tcpConnectHelper)
+
+
+        // 전체화면 설정
+        hideSystemBars()
+
+
+        // CameraXHelper 초기화
+        cameraXHelper = CameraXHelper(this, binding.previewView) { imageProxy ->
+            val mpImage = cameraXHelper.imageProxyToMPImage(imageProxy)  // CameraXHelper의 함수 호출
+            mpImage?.let {
+                val timestampMs = imageProxy.imageInfo.timestamp / 1000L
+                handTrackerHelper.detectHands(mpImage, timestampMs)
+            }
+            imageProxy.close()
+        }
+
+        orientationHelper = OrientationHelper(this)
+
+        cameraXHelper.startCamera()
+
+        //카메라 전환 버튼
+        binding.switchCameraButton.setOnClickListener {
+            cameraXHelper.switchCamera()
+        }
+
+        // TCP 연결 버튼 클릭 리스너
+        connectTcpButton.setOnClickListener {
+            val address = tcpAddressEditText.text.toString().split(":")
+            if (address.size == 2) {
+                val ip = address[0]
+                val port = address[1].toInt()
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    tcpConnectHelper.connectTcpServer(ip, port)
+                    Toast.makeText(this@MainActivity, "TCP 서버 연결 시도 중: $ip:$port", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "올바른 TCP 주소를 입력하세요 (예: 192.168.0.2:8080)", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraXHelper.stopCamera()  // 카메라 종료
+        tcpConnectHelper.closeConnection()
+    }
+
+
+    private fun hideSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // API 30 이상 (안드로이드 11 이상)
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.systemBars())  // 상태바, 네비게이션 바 숨김
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE  // 스와이프로 바 보이기
+            }
+        } else {
+            // API 30 미만 (안드로이드 10 이하)
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionHelper.handlePermissionResult(
+            requestCode,
+            grantResults,
+            onGranted = {
+                cameraXHelper.startCamera()  // 카메라 권한 허용 시 카메라 실행
+            },
+            onDenied = {
+                Toast.makeText(this, "카메라 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+}
---
+++ airpoint_server/tcp_reciever.py
@@ -0,0 +1,69 @@
+import socket
+import json
+import copy
+
+from PyQt5.QtCore import pyqtSignal, QObject
+
+class DataReceiver(QObject):
+    data_received = pyqtSignal(dict)  # 데이터를 보내는 시그널
+
+    def start_server(self, host='0.0.0.0', port=25565):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((host, port))
+        server_socket.listen(1)
+        client_socket, addr = server_socket.accept()
+
+        while True:
+            data = receive_data(client_socket)
+            if data:
+                self.data_received.emit(data)  # 데이터를 전달
+
+                
+def receive_data(client_socket):
+    BUFFER_SIZE = 1024  # 버퍼 크기
+    data = b""  # 받은 데이터를 저장할 변수
+
+    # 먼저 길이 정보(10바이트 고정) 수신
+    length_data = client_socket.recv(10)
+    if not length_data:
+        return None  # 길이 정보가 없으면 종료
+
+    # 길이 정보가 올바른지 확인
+    data_length = int(length_data.decode('utf-8').strip())
+
+    # 데이터를 받기 위한 루프
+    while len(data) < data_length:
+        packet = client_socket.recv(min(BUFFER_SIZE, data_length - len(data)))  # 남은 길이만큼 받기
+        if not packet:
+            break  # 데이터가 없으면 종료
+        data += packet  # 받은 데이터를 계속 추가
+
+    if len(data) == data_length:
+        return json.loads(data)  # JSON 파싱하여 반환
+    return None  # 데이터가 올바르게 수신되지 않으면 None 반환
+
+def start_server(host='0.0.0.0', port=25565):
+    """서버 시작 함수"""
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print("서버 대기 중...")
+
+    client_socket, addr = server_socket.accept()
+    print(f"{addr} 연결됨!")
+
+    prevData = None
+    currentData = None
+
+    delay = 0
+    while True:
+        data = receive_data(client_socket)
+        if not data:
+            print("연결 끊김")
+            break
+
+        print(f"수신된 데이터: {data}")
+
+
+    client_socket.close()
+    server_socket.close()
